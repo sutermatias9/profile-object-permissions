4c5891702ee9687f30f058a8ff8fd4c82a7dabbe
Matias Suter <sutermatias9@gmail.com>
2024-04-27 18:02:10 -0300

    Show profiles name in combobox

--- force-app/main/default/lwc/dataViewer/dataViewer.js
+++ force-app/main/default/lwc/dataViewer/dataViewer.js
@@ -1,3 +1,25 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import getProfiles from '@salesforce/apex/ProfileHandler.getProfiles';
 
-export default class DataViewer extends LightningElement {}
+export default class DataViewer extends LightningElement {
+    profileOptions;
+    profileSelected;
+
+    // TODO Get profiles
+    @wire(getProfiles)
+    wiredProfiles({ data, error }) {
+        if (data) {
+            // Profile combobox options
+            this.profileOptions = data.map((p) => {
+                return { label: p.Name, value: p.Name };
+            });
+        } else if (error) {
+            console.error(error);
+        }
+    }
+
+    handleProfileChange(event) {
+        this.profileSelected = event.detail.value;
+        console.log(JSON.stringify(this.profileSelected));
+    }
+}
--- force-app/main/default/lwc/objectPermissionsAndData/objectPermissionsAndData.js
+++ force-app/main/default/lwc/objectPermissionsAndData/objectPermissionsAndData.js
@@ -1,7 +1,28 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import getProfiles from '@salesforce/apex/ProfileHandler.getProfiles';
 
 export default class ObjectPermissionsAndData extends LightningElement {
+    profileOptions;
+    profileSelected;
+
     // TODO Get profiles
+    @wire(getProfiles)
+    wiredProfiles({ data, error }) {
+        if (data) {
+            // Profile combobox options
+            this.profileOptions = data.map((p) => {
+                return { label: p.Name, value: p.Name };
+            });
+        } else if (error) {
+            console.error(error);
+        }
+    }
+
+    handleProfileChange(event) {
+        this.profileSelected = event.detail.value;
+        console.log(JSON.stringify(this.profileSelected));
+    }
+
     // TODO Get objects
     // TODO Make table
     // TODO Show fields when button click
