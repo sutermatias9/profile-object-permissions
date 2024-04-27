2db8fae307353cb386687d1c9f59f8fe85021ce5
Matias Suter <sutermatias9@gmail.com>
2024-04-27 17:28:10 -0300

    Basic Structure

---
+++ force-app/main/default/lwc/objectPermissionsAndData/objectPermissionsAndData.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+
+export default class ObjectPermissionsAndData extends LightningElement {
+    // TODO Get profiles
+    // TODO Get objects
+    // TODO Make table
+    // TODO Show fields when button click
+    // TODO Compare permissions with 2nd profile
+}
