9668f4ae7b7a56581d28fa359f1b727093f319bd
Matias Suter <sutermatias9@gmail.com>
2024-06-25 18:05:17 -0300

    getPermissions method created

--- force-app/main/default/lwc/fieldPermissionsToggles/fieldPermissionsToggles.js
+++ force-app/main/default/lwc/fieldPermissionsToggles/fieldPermissionsToggles.js
@@ -3,17 +3,32 @@ import { LightningElement, api } from 'lwc';
 export default class FieldPermissionsToggles extends LightningElement {
     @api permissions;
 
+    @api getPermissions() {
+        const containers = Array.from(this.template.querySelectorAll('.toggles-container'));
+
+        const permissions = containers.map((container) => {
+            const [readToggle, editToggle] = this.getToggles(container);
+            const field = readToggle.dataset.field;
+
+            return { field, read: readToggle.checked, edit: editToggle.checked };
+        });
+
+        return permissions;
+    }
+
     handlePermissionChange(event) {
         const { label: permission, checked } = event.currentTarget;
         const field = event.currentTarget.dataset.field;
         const [readToggle, editToggle] = this.template.querySelectorAll(`lightning-input[data-field="${field}"]`);
-        console.log(readToggle);
-        console.log(editToggle);
 
         if (permission === 'Read' && !checked) {
             editToggle.checked = false;
         } else if (permission === 'Edit' && checked) {
             readToggle.checked = true;
         }
+    }
+
+    getToggles(container) {
+        return container.querySelectorAll('lightning-input');
     }
 }
